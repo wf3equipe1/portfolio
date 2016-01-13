52983901e1a974e190bef2c6dc1b814d1a9788c8
romaingaspe <rom.gaspe@gmail.com>
2016-01-13 12:16:13 +0100

    correct

--- admin/nouvelutilisateur.php
+++ admin/nouvelutilisateur.php
@@ -68,13 +68,13 @@ if (!empty($post)) {
 
 
 	<form method="POST">
-		<label for="pseudo">pseudo</label>
+		<label for="pseudo">Pseudo</label>
 		<input type="text" name="pseudo" id="pseudo" placeholder="Votre pseudo">
 		<br>
-		<label for="email">email</label>
-		<input type="text" name="email" id="email" placeholder="votre@email.fr">
+		<label for="email">Email</label>
+		<input type="email" name="email" id="email" placeholder="votre@email.fr">
 		<br>
-		<label for="password">mot de passe</label>
+		<label for="password">Mot de passe</label>
 		<input type="password" name="password" id="password">
 		<br>
 		<input type="submit" value="Envoyer">
