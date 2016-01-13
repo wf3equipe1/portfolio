86e216af0f02e26649b4752bc78f4ef655442a9f
samuelJuzan <samuel.juzan@orange.fr>
2016-01-13 11:35:43 +0100

    Index retouché et debut menu gauche

---
+++ composants/menugauche.php
@@ -0,0 +1,18 @@
+<section id="leftSide">
+	<header>
+		<img src="" alt="clientPicture">
+		<ul>
+			<li>Nom Prenom</li>
+			<li>tel :06.12.34.56.78</li>
+			<li>email:email@contatct.me</li>
+		</ul>
+	</header>
+
+	<nav>
+		<ul>
+			<li><a href="index.php">Accueil</a></li>
+			<li><a href="actualites.php">Les news</a></li>
+			<li><a href="contact.php">Contact</a></li>
+		</ul>
+	</nav>
+</section>
--- index.php
+++ index.php
@@ -1,13 +1,33 @@
-<?php
-//Page d'accueil du site
+<?php//Page d'accueil du site
+session_start();
 require_once 'composants/db.php';
-
-include_once 'composants/menugauche.php';
-
 // Titre
-
 // Photo de couverture
-
 // Affichage des news
-
  ?>
+
+ <!DOCTYPE html>
+ <html>
+ <head>
+ 	<title>Page principale</title>
+ 	<meta charset="utf-8">
+ 	<link rel="stylesheet" href="css/style.css">
+ </head>
+ <body>
+	 <main>
+	 	<?php include_once 'composants/menugauche.php'; //MENU DE GAUCHE?>
+	 	
+	 	<section id="rightSide">
+	 		<header>
+	 			<h1>Titre Portfolio</h1>
+
+	 			<div id="banner"></div>
+	 		</header>
+	 		<div id="newsIndex">
+	 			<h1>Les news</h1>
+
+	 		</div>
+	 	</section>
+	 </main>
+ </body>
+ </html>
