f8d575c3374939a9c94d610c404d146dd8fe6280
Manu <ev@njibhu.eu>
2016-01-13 10:20:52 +0100

    Déplacement de recherche dans une page actualité

---
+++ index.php
@@ -0,0 +1,13 @@
+<?php
+//Page d'accueil du site
+require_once 'composants/db.php';
+
+include_once 'composants/menugauche.php';
+
+// Titre
+
+// Photo de couverture
+
+// Affichage des news
+
+ ?>
