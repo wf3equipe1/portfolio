0df7a1e34cdc8f15cf3838c8a8388474dd3baaf6
Manu <ev@njibhu.eu>
2016-01-14 15:33:28 +0100

    Fin de pagination

--- actualites.php
+++ actualites.php
@@ -12,6 +12,8 @@ $get = array_map('cleandata', $_GET);
 $errors = array();
 $validArticles = false;
 
+$tailleDePagination = 10;
+
 //Page 1 par défaut.
 if(!isset($get['page'])){
     $get['page'] = 1;
@@ -23,21 +25,27 @@ if(isset($get['page'])){
         if($get['page'] == 0){
             $get['page'] = 1;
         }
-        $offset = ($get['page'] * 10) - 10;
+        $offset = ($get['page'] * $tailleDePagination) - $tailleDePagination;
     } else {
         $offset = 0;
     }
 
     //Requête spécifique a la recherche
     if(isset($get['search'])){
-        $req = $pdo_database->prepare('SELECT * FROM articles WHERE content LIKE :recherche ORDER BY date DESC LIMIT 10 OFFSET :offset');
+        $nbarticles_req = $pdo_database->prepare('SELECT COUNT(*) FROM articles WHERE content LIKE :recherche');
+        $req = $pdo_database->prepare('SELECT * FROM articles WHERE content LIKE :recherche ORDER BY date DESC LIMIT :size OFFSET :offset');
         $req->bindValue(':recherche', '%'.$get['search'].'%', PDO::PARAM_STR);
+        $nbarticles_req->bindValue(':recherche', '%'.$get['search'].'%', PDO::PARAM_STR);
     } else {
         //Requête sans la recherche
-        $req = $pdo_database->prepare('SELECT * FROM articles ORDER BY date DESC LIMIT 10 OFFSET :offset');
+        $req = $pdo_database->prepare('SELECT * FROM articles ORDER BY date DESC LIMIT :size OFFSET :offset');
+        $nbarticles_req = $pdo_database->prepare('SELECT COUNT(*) FROM articles');
     }
+    $req->bindValue(':size', $tailleDePagination, PDO::PARAM_INT);
     $req->bindValue(':offset', $offset, PDO::PARAM_INT);
     if($req->execute()){
+        $nbarticles_req->execute();
+        $nbarticles = $nbarticles_req->fetchColumn();
         $articles = $req->fetchAll();
         if(count($articles)==0){
             $errors[] = 'Aucun résultat.';
@@ -94,6 +102,18 @@ if(isset($get['page'])){
                 </article>
                 <?php endforeach;?>
                 <?php //Création des liens page suivante et page précédente.
+                    //Affichage des pages
+                    echo '<p>';
+                    $nbpages = ceil($nbarticles/$tailleDePagination);
+                    for($i=1; $i <= $nbpages; $i++){
+                        if(isset($get['search'])){ //Lien avec paramètre de recherche
+                            echo '<a href="actualites.php?page='.$i.'&search='.$get['search'].'">'.$i.'</a>';
+                        } else { //Lien sans paramètre de recherche
+                            echo '<a href="actualites.php?page='.$i.'">'.$i.'</a>';
+                        }
+                    }
+                    echo '</p>';
+
                     if($offset>0){
                         $prev = $get['page'] - 1;
                         if(isset($get['search'])){ //Lien avec paramètre de recherche
@@ -102,13 +122,16 @@ if(isset($get['page'])){
                             echo '<p><a href="actualites.php?page='.$prev.'">Page précédente</a></p>';
                         }
                     }
-                    $next = $get['page'] + 1;
-                    if(isset($get['search'])){ //Lien avec paramètre de recherche
-                        echo '<p><a href="actualites.php?page='.$next.'&search='.$get['search'].'">Page suivante</a></p>';
-                    } else { //Lien sans paramètre de recherche
-                        echo '<p><a href="actualites.php?page='.$next.'">Page suivante</a></p>';
-                    }
 
+                    //Affiche la page suivante uniquement si la page suivante contient des articles ayant un offset inferieur au nombre maximal
+                    if($offset + $tailleDePagination < $nbarticles){
+                        $next = $get['page'] + 1;
+                        if(isset($get['search'])){ //Lien avec paramètre de recherche
+                            echo '<p><a href="actualites.php?page='.$next.'&search='.$get['search'].'">Page suivante</a></p>';
+                        } else { //Lien sans paramètre de recherche
+                            echo '<p><a href="actualites.php?page='.$next.'">Page suivante</a></p>';
+                        }
+                    }
                  ?>
             </div>
             <?php elseif(count($errors)>0): ?>
