7403f097db8b83b58a9c0dc74b62d5834fdd4f39
bert189 <bertrandmuscat@gmail.com>
2016-01-15 10:09:35 +0100

    correction barre admin

--- composants/barreadmin.php
+++ composants/barreadmin.php
@@ -2,35 +2,36 @@
 
 // pour l'affichage en français
 
+if($_SESSION['isconnected']):
 
+	if($_SESSION['role'] == 'editor'){
+		$role = 'éditeur';
+	}
+	else {
+		$role = 'administrateur';
+	}
+	?>
+	<header>
+		<div>
+			<p>bonjour <span><?php echo $_SESSION['username']?></span>, vous êtes connecté en tant qu'<span><?php echo $role?></span></p>
+		</div>
+		<nav>
+			<ul>
+				<li><a href="nouvelarticle.php">Nouvel article</a></li>
+				<li><a href="actualites.php">Modifier un article</a></li>
+				
+				<?php if ($_SESSION['role'] == 'admin'): // réservé à l'admin ?>
 
-if($_SESSION['role'] == 'editor'){
-	$role = 'éditeur';
-}
-else {
-	$role = 'administrateur';
-}
-?>
-<header>
-	<div>
-		<p>bonjour <span><?php echo $_SESSION['username']?></span>, vous êtes connecté en tant qu'<span><?php echo $role?></span></p>
-	</div>
-	<nav>
-		<ul>
-			<li><a href="nouvelarticle.php">Nouvel article</a></li>
-			<li><a href="actualites.php">Modifier un article</a></li>
-			
-			<?php if ($_SESSION['role'] == 'admin'): // réservé à l'admin ?>
+				<li><a href="contactgestion.php">Messages</a></li>
+				<li><a href="nouvelutilisateur.php">Nouvel utilisateur</a></li>
+				<li><a href="listeutilisateurs.php">Utilisateurs</a></li>
+				<li><a href="options.php">Options</a></li>
 
-			<li><a href="contactgestion.php">Messages</a></li>
-			<li><a href="nouvelutilisateur.php">Nouvel utilisateur</a></li>
-			<li><a href="listeutilisateurs.php">Utilisateurs</a></li>
-			<li><a href="options.php">Options</a></li>
-
-			<?php endif; ?>
-			
-			<li><a href="motdepasse">Modifier votre mot de passe</a></li>
-			<li><a href="index.php?logout">Vous deconnecter</a></li>
-		</ul>
-	</nav>
-</header>
+				<?php endif; ?>
+				
+				<li><a href="motdepasse.php">Modifier votre mot de passe</a></li>
+				<li><a href="index.php?logout">Vous deconnecter</a></li>
+			</ul>
+		</nav>
+	</header>
+<?php endif; ?>
